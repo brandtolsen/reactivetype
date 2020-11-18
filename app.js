58edb691902b7b932ff55fbbaf2cc3ecbee7a3a2
Daniel Brandt-Olsen <daniel@brandtolsen.com>
2020-11-18 17:49:52 +0100

    cursor to tag and calculate distance

--- app.js
+++ app.js
@@ -6,5 +6,9 @@ document.addEventListener("mousemove", function (event) {
         const dx = div.offsetLeft + 50 - x
         const dy = div.offsetTop + 50 - y
         const dist = Math.sqrt(dx * dx + dy * dy)
+        const score = Math.exp(dist * -0.003)
+
+        div.style.transform = "scale(" + score + ")"
+        div.style.fontWeight = 100 + (100 * Math.round(8 * score))
     })    
 })
